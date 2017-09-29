2deec9890f9b9d8353a3148fe24b2d1de68a4354
mass10 <mass10.github@gmail.com>
2017-09-29 20:15:11 +0900

    wip

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,7 @@
 public final class App {
 
     public String getGreeting() {
-        return "Hello world."; //★コンパイルに失敗するファイル
+        return "*** Hello world. ***"; //★コンパイルに失敗するファイル
     }
 
     public static void main(String[] args) {
